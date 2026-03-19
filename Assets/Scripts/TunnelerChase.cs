0ee5c5bad38098c735a89066fd93b41f069410a6
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2026-03-18 17:00:01 -0700

    fixed tunneler flashing and idle animation

--- Assets/Scripts/TunnelerChase.cs
+++ Assets/Scripts/TunnelerChase.cs
@@ -5,25 +5,28 @@ public class TunnelerChase : MonoBehaviour
     public float speed;
     public float firerate;
     Rigidbody2D rb;
-    //public Renderer tunnelerBodyRenderer;
     public Animator tunnelerBodyAnimator;
     Transform player;
     GameObject tunnelerBody;
+    public SpriteRenderer tunnelerBodySpriteRenderer;
+    public CircleCollider2D tunnelerBodyCollider;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
         tunnelerBody = transform.GetChild(0).gameObject;
         tunnelerBodyAnimator = tunnelerBody.GetComponent<Animator>();
-        //tunnelerBodyRenderer = tunnelerBody.GetComponent<Renderer>();
+        tunnelerBodySpriteRenderer = tunnelerBody.GetComponent<SpriteRenderer>();
+        tunnelerBodyCollider = tunnelerBody.GetComponent<CircleCollider2D>();
         StartCoroutine(Movement());
     }
     IEnumerator Movement()
     {
         while(true)
         {
-            tunnelerBody.SetActive(false);
-            for(int i=0; i<1; i++)
+            tunnelerBodySpriteRenderer.enabled = false;
+            tunnelerBodyCollider.enabled = false;
+            for (int i=0; i<4; i++)
             {
                 float t = 0f;
                 while (t < 0.8f)
@@ -35,18 +38,15 @@ public class TunnelerChase : MonoBehaviour
                 }
                 yield return new WaitForSeconds(0.6f);
             }
-            yield return new WaitForSeconds(0.65f);
-            tunnelerBody.SetActive(true);
-            tunnelerBodyAnimator.SetBool("ActivateIdle 0", true);
-            print("idle");
+            yield return new WaitForSeconds(0.70f);
+            tunnelerBodySpriteRenderer.enabled = true;
+            tunnelerBodyCollider.enabled = true;
+            tunnelerBodyAnimator.SetBool("FlashingRed", false);
             yield return new WaitForSeconds(0.3f);
-            tunnelerBodyAnimator.SetBool("ActivateIdle 0", false);
-            tunnelerBodyAnimator.SetBool("FlashRed 0",true);
-            print("flashred");
-            yield return new WaitForSeconds(1.5f);
-            tunnelerBodyAnimator.SetBool("FlashRed 0", false);
-            tunnelerBodyAnimator.SetBool("ActivateIdle 0", true);
-            print("idle");
+            tunnelerBodyAnimator.SetBool("FlashingRed",true);
+            yield return new WaitForSeconds(1f);
+            tunnelerBodyAnimator.SetBool("FlashingRed", false);
+            yield return new WaitForSeconds(1.2f);
         }
     }
 }
